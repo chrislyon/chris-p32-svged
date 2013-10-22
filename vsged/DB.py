fd4d494a196656b05b107eccd84185287db6471c
chris <chris.lyon.tech@gmail.com>
2013-10-22 16:05:46 +0200

    ya encore de l'analyse a faire

--- vsged/DB.py
+++ vsged/DB.py
@@ -1,9 +1,58 @@
+## ----------------------------------------
+## Test de stockage de fichier dans mongodb
+## ----------------------------------------
+
 from pymongo import MongoClient, collection
+import datetime
 
 SERVER='localhost'
 PORT=27017
 
+def log(msg="<vide>"):
+	"""
+	Petite routine de log
+	"""
+	d = datetime.datetime.now()
+	ts = d.strftime("%j %X %f")
+	print("%s : %s" % (ts, msg) )
+
+
+class Horodatage(object):
+	"""
+	Classe pour stocker les evenements horodatés
+	"""
+	def __init__(self):
+		self.dh_creation = None
+		self.dh_modification = None
+		self.last_access_read = None
+		self.last_access_write = None
+
+class Proprietaire(object):
+	"""
+	Classe pour stocker les attributs propriétaires
+	"""
+	def __init__(self):
+		self.user = None
+		self.group = None
+
+class repertoire(object):
+	"""
+	La classe répertoire
+	est un conteneur
+
+	"""
+	pass
+
+class fichier(object):
+	"""
+	La classe fichier
+	"""
+	pass
+
 class DB(object):
+	"""
+	La classe DB Database 
+	"""
 	def __init__(self, name):
 		self.name = name
 		self.db_cli = MongoClient(SERVER, PORT)
@@ -11,11 +60,25 @@ class DB(object):
 		self.db = self.db_cli[name]
 		try:
 			self.doc = self.db['DOC']
+			log("Modele cree ...")
 		except:
 			self.doc = self.create_modele()
+			log("Creation du modele ...")
 
 	def __str__(self):
 		return "DB:%s %s" % (self.name, self.db.collection_names())
+
+	def create_dir(self, rep):
+		"""
+		Creation d'un repertoire
+		"""
+		pass
+
+	def create_file(self, fic):
+		"""
+		creation d'un fichier
+		"""
+		pass
 
 	def create_modele(self):
 		d = { 'nom':'root', 'meta':{
